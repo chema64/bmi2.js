3f92baaf62fd920f418662258b2d7270d54e8c91
chema64 <chema64@gmail.com>
2024-01-29 12:36:38 +1000

    kg and meter text input added to html. addEventListener added to button.

---
+++ index.js
@@ -0,0 +1,30 @@
+function bmiCalculator (weight, height) {
+    var interpretation ="";
+    var bmi = Math.round(weight / Math.pow(height, 2));
+    if(bmi < 18.5) {
+        interpretation = "Your BMI is "+bmi+", so you are underweight.";
+    }
+     if(bmi > 18.5 && bmi < 24.9) {
+        interpretation = "Your BMI is "+bmi+" , so you have a normal weight.";
+    }   
+    if(bmi > 24.9) {
+        interpretation = "Your BMI is "+bmi+", so you are overweight.";
+    }
+    // console.log(interpretation);
+    return interpretation;
+    
+}
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", handleClick);
+
+function handleClick() {
+    console.log(bmiCalculator(document.querySelector("#weightkg").value, document.querySelector("#heightm").value));
+   document.querySelector("#title1").innerHTML = bmiCalculator(document.querySelector("#weightkg").value, document.querySelector("#heightm").value);
+
+}
+
+// var result = "";
+
+// result = bmiCalculator(80, 1.75);
